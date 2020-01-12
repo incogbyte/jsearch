f7e1992624a65739a9bf718247e1b1c2356eebf0
inc0gbyt3 <53656948+incogbyte@users.noreply.github.com>
2020-01-12 15:27:57 -0300

    add vtex and email regex
    vtex eccomerce api credencials..

--- regex_modules/regex_modules.py
+++ regex_modules/regex_modules.py
@@ -7,8 +7,9 @@ REGEX_PATT = {
     "AMAZON_URL_4":r"s3-[a-z0-9-]+\.amazonaws\\.com/[a-z0-9._-]+",
     "URLS":r"https?://[^\"\\'> ]+",
     "AMAZON_KEY":r"([^A-Z0-9]|^)(AKIA|A3T|AGPA|AIDA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{12,}",
-    "UPLOAD_FIELDS":r"\u003cinput[^\u003e]+type=[\"']?file[\"']?",
     "Authorization":r"^Bearer\s[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",
-    "accessToken":r"^acesstoken=[0-9]{13,17}"
+    "accessToken":r"^acesstoken=[0-9]{13,17}",
+    "vtex-key":r"vtex-api-(appkey|apptoken)",
+    "email":r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
     
 }
